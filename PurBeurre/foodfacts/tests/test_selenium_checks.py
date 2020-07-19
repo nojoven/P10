f533cb9c7dff78fee19d3491e64b3dd8a6206bc8
nojoven <work.cedricjoseph@gmail.com>
2020-07-19 18:23:10 +0000

    edition of yml file

--- PurBeurre/foodfacts/tests/test_selenium_checks.py
+++ PurBeurre/foodfacts/tests/test_selenium_checks.py
@@ -11,7 +11,11 @@ class MySeleniumTests(StaticLiveServerTestCase):
     This class provides a configuration and a set of actions to
     simulate the behaviour of a human user.
     """
+
     options = webdriver.ChromeOptions()
+    options.add_argument('--no-sandbox')
+    options.add_argument('--headless')
+    options.add_argument('--disable-dev-shm-usage')
     options.add_argument("--disable-extensions")
     driver = webdriver.Chrome(options=options)
     driver.implicitly_wait(30000)
