bbfd824129490d9789759743562575bed160f99b
cedricjoseph <work.cedricjoseph@gmail.com>
2020-07-23 19:45:43 +0200

    test

--- PurBeurre/PurBeurre/settings.py
+++ PurBeurre/PurBeurre/settings.py
@@ -17,8 +17,7 @@ import raven
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
 
-
-# import psycopg2.extensions
+import psycopg2.extensions
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
