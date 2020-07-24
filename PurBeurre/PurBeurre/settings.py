2c4b9d82a68f8bb425751d595e38d99f042498a0
cedricjoseph <work.cedricjoseph@gmail.com>
2020-07-24 22:02:59 +0200

    t

--- PurBeurre/PurBeurre/settings.py
+++ PurBeurre/PurBeurre/settings.py
@@ -94,8 +94,8 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
         "OPTIONS": {},
         "NAME": "purbeurre",
-        "USER": "root",
-        "PASSWORD": "Hamzamal89",
+        "USER": "postgres",
+        "PASSWORD": "",
         "HOST": "127.0.0.1",
         "PORT": "5432",
     }
