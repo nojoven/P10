6160174344b6f8e5406cce2f7195f2a0cc3102f5
cedricjoseph <work.cedricjoseph@gmail.com>
2020-07-27 20:29:15 +0200

    test

--- PurBeurre/foodfacts/tests/test_selenium_checks.py
+++ PurBeurre/foodfacts/tests/test_selenium_checks.py
@@ -29,7 +29,7 @@ class MySeleniumTests(StaticLiveServerTestCase):
         This contains the list of actions to perform in the browser,
          the order in which Selenium executes them and the assertions to test.
         """
-        self.databases.append("")
+        
         logging.info(Favorites.objects.all())
         print(Favorites.objects.all())
 
