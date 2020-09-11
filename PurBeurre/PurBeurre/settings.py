ed794a9a882966df36d3f634aa6e0c4216dc5631
cedricjoseph <work.cedricjoseph@gmail.com>
2020-09-11 23:12:40 +0200

    t

--- PurBeurre/PurBeurre/settings.py
+++ PurBeurre/PurBeurre/settings.py
@@ -97,8 +97,8 @@ DATABASES = {
         "NAME": "purbeurre",
         "USER": "postgres",
         "PASSWORD": "",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
+        "HOST": f"{LiveServerTestCase.allowed_host}",
+        "PORT": "5433",
     }
 }
 
